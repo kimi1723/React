08deace44942fb7061198951abd38d34452d17e6
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-06-12 21:14:23 +0200

    l

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem  from "./components/ExpenseItem";
+
+
 function App() {
   return (
     <div>
+      <h1>es2</h1>
       <h2>Let's get started!</h2>
+      <ExpenseItem/>
     </div>
   );
 }
