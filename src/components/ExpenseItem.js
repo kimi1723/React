2b17e594e24f7f8a2314eda5bd8a978cc3c4a97c
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-06-20 14:34:31 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@ const ExpenseItem = (props) => {
 
     return(
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>{props.date.toISOS+tring()}</div>
              <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
